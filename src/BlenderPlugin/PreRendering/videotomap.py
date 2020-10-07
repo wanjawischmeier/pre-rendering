02dab71a18cdd23972ed0a00cb0b24deb9f5dc73
wanjawischmeier <69650445+wanjawischmeier@users.noreply.github.com>
2020-10-07 21:36:30 +0200

    VideoToMap working, added gitignore

---
+++ src/BlenderPlugin/PreRendering/videotomap.py
@@ -0,0 +1,38 @@
+from json import dumps as serialize, loads as deserialize
+from os import getcwd
+from os.path import splitext
+
+def videoToMap(videoPath: str, targetPath: str, mapWidth: int):
+    data = {
+        "width":    mapWidth,
+        "tstvalue": "whatever" 
+    }
+
+    data_bytes = bytearray(serialize(data), "utf-8")
+    data_bytes.extend(bytearray("\n", "utf-8"))
+
+    with open(videoPath, "rb") as videoBinary:
+        data_bytes.extend(videoBinary.read())
+
+    with open(splitext(targetPath)[0] + ".prm", "wb") as mapFile:
+        mapFile.write(data_bytes)
+
+
+def getMapData(mapPath: str, targetVideoPath: str) -> dict:
+    with open(splitext(mapPath)[0] + ".prm", "rb") as binaryFile:
+        binaryData = binaryFile.read()
+
+    json_data, sep, video = binaryData.partition(bytearray("\n", "utf-8"))
+    data = deserialize(json_data.decode("utf-8"))
+
+    with open(targetVideoPath, "wb") as videoBinary:
+        videoBinary.write(video)
+
+    return data
+
+"""
+path = getcwd() + "\\src\\BlenderPlugin\\PreRendering"
+
+videoToMap(path + "\\TestVideo.mp4", path + "\\TestMap.map", 24)
+output = getMapData(path + "\\TestMap.map", path + "\\OutVideo.mp4")
+"""
