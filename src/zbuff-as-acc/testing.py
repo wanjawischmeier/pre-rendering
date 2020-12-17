b3b0bb57b6d53f66d49d11a1ce03546b169dc9be
wanjawischmeier <69650445+wanjawischmeier@users.noreply.github.com>
2020-12-17 11:02:05 +0100

    expanding data sheet

--- src/zbuff-as-acc/testing.py
+++ src/zbuff-as-acc/testing.py
@@ -9,7 +9,7 @@ with wave.open(path, 'rb') as wav:
 
 '''
 Action          |   Value       |   Unit
-_________________________________________
+__________________________________________
                     AAC std:
 samplerate      |   44.100      |   Hz
 max samplerate  |   48.000
@@ -17,23 +17,35 @@ samples p. s.   |   44.100      |   S/s
 sample size     |   16          |   bits
 bitrate         |   95.000      |   Bit/s
 max bitrate     |   2
------------------------------------------
+------------------------------------------
                     BW HD IMG:
 bw hd image     |   921.600     |   bytes
 framerate       |   30          |   fps
 pixel size      |   8           |   bits
 bytes - second  |   27.648.000  |   bytes
 bits - second   |   221.184.000 |   Bit/s
------------------------------------------
+------------------------------------------
                     BW 480p IMG:
 bw hd image     |   172.800     |   bytes
 framerate       |   30          |   fps
 pixel size      |   8           |   bits
 bytes - second  |   5.184.000   |   bytes
 bits - second   |   41.472.000  |   Bit/s
------------------------------------------
+------------------------------------------
                     needed:
 samplerate      |   27.648.000  |   Hz
 sample size     |   8           |   bits
 bitrate         |   221.184.000 |   Bit/s
+------------------------------------------
+                    nested:
+8-bit           |   256 (0-255) |   possib
+16-bit          |   65.536      |   possib
+32-bit          |   4294967296  |   possib
+
+
+Iteration   |   max value   |   width
+__________________________________________
+                16 -> 8 bit
+1st         |   255         |   65.280
+2nd         |   255         |   16.711.680
 '''
