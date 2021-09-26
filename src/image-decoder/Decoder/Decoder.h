6a7a0e0a614d07931a216b677d2c17e46d1ae3d7
wanjawischmeier <69650445+wanjawischmeier@users.noreply.github.com>
2021-09-26 12:57:21 +0200

    archived decoding progress

--- src/image-decoder/Decoder/Decoder.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#define DECODER __declspec(dllimport)
-
-#include <iostream>
-#include <opencv2\opencv.hpp>
-
-extern "C" DECODER unsigned char* GetUnsignedBytes(char* image, int* bytes_count, bool* debug);
