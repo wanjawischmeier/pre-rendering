d911ddca235c365ac69ca35b075dd2700771b6b1
wanjawischmeier <69650445+wanjawischmeier@users.noreply.github.com>
2021-10-14 21:56:50 +0200

    flattening testing

--- decoder/py-decoder/main.py
+++ decoder/py-decoder/main.py
@@ -1,11 +1,11 @@
 from cv2 import imread, imshow, resize, split, waitKey, IMREAD_UNCHANGED
 
-path = "C:\\Users\\wanja\\Documents\\dev\\pre-rendering\\master\\src\\unity-concept\\Assets\\Rendering\\Testing\\Sample1\\Main.png"
+path = "C:\\Users\\wanja\\Documents\\dev\\pre-rendering\\master\\src\\unity\\concept\\Assets\\Rendering\\Testing\\Sample1\\Main.png"
 
 img = imread(path, IMREAD_UNCHANGED)
-img = resize(img, (800, 400))
+img = resize(img, (8, 10))
 a = split(img)[3]
-
+b = img.flatten()
 imshow("Image", img)
 imshow("A", a)
 waitKey(0)
