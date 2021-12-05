2a277ce7a79c364498ce3fe04df91bc0dda8e26a
wanjawischmeier <69650445+wanjawischmeier@users.noreply.github.com>
2021-12-05 12:20:29 +0100

    Update main.py

--- decoder/py-decoder/main.py
+++ decoder/py-decoder/main.py
@@ -1,11 +1,11 @@
-from cv2 import imread, imshow, resize, split, waitKey, IMREAD_UNCHANGED
+from cv2 import COLOR_GRAY2BGRA, imread, imshow, split, resize, vconcat, cvtColor, waitKey, IMREAD_UNCHANGED
+from pg
 
-path = "C:\\Users\\wanja\\Documents\\dev\\pre-rendering\\master\\src\\unity\\concept\\Assets\\Rendering\\Testing\\Sample1\\Main.png"
+path = "C:\\Users\\wanja\\Documents\\dev\\pre-rendering\\renders\\testing\\jpeg-2000\\test1.png"
 
 img = imread(path, IMREAD_UNCHANGED)
-img = resize(img, (8, 10))
+img = resize(img, (1920, 540))
 a = split(img)[3]
 b = img.flatten()
-imshow("Image", img)
-imshow("A", a)
+imshow("Image", vconcat([img, cvtColor(a, COLOR_GRAY2BGRA)]))
 waitKey(0)
