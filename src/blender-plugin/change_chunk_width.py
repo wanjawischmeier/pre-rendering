35be3f39cebc504aeb36f2546607ee33a30bb7e2
wanjawischmeier <69650445+wanjawischmeier@users.noreply.github.com>
2022-01-23 12:50:04 +0100

    Update change_chunk_width.py

--- src/blender-plugin/change_chunk_width.py
+++ src/blender-plugin/change_chunk_width.py
@@ -1,28 +1,14 @@
 import bpy
 
-chunkWidthOld = 4
+# chunkWidthOld = 4
+heightOld = 21
 chunkWidth = 4
 chunkColumns = 5
 chunkRows = 5
+channelBlocks = 2
 
-def calculateOldFrameOffset(a,b):
-    frame: int = bpy.context.scene.frame_current
-
-    y = frame%(chunkWidth*chunkColumns)
-    x = (frame-y)/(chunkWidth*chunkColumns)
-
-    localIndex = x%chunkWidthOld + y%(chunkWidthOld**2)
-    chunkIndex = (frame-localIndex)/chunkWidthOld
-    targetOffset = localIndex+chunkIndex-frame
-
-    bpy.app.driver_namespace['frame'] = frame
-    bpy.app.driver_namespace['x'] = x
-    bpy.app.driver_namespace['y'] = y
-    bpy.app.driver_namespace['localIndex'] = localIndex
-    bpy.app.driver_namespace['chunkIndex'] = chunkIndex
-    bpy.app.driver_namespace['frameOffset'] = targetOffset
-
-
+# not properly working yet
+"""
 def calculateFrameOffset(a,b):
     frame: int = bpy.context.scene.frame_current
     domainLocation = [0, 0]
@@ -53,10 +39,47 @@ def calculateFrameOffset(a,b):
 
     localIndex = absolutePosition[0]%chunkWidthOld + absolutePosition[1]%(chunkWidthOld**2)
     chunkIndex = (frame-localIndex)/chunkWidthOld
-    targetOffset = localIndex+chunkIndex-frame
+    targetOffset = localIndex+chunkIndex
 
     bpy.app.driver_namespace['frameOffset'] = targetOffset
 
 handler = bpy.app.handlers.frame_change_pre
 handler.clear()
 handler.append(calculateOldFrameOffset)
+"""
+
+node_tree = bpy.data.scenes['Scene'].node_tree
+image_node = node_tree.nodes['Image']
+mix_node_fac = node_tree.nodes["Mix"].inputs[0]
+
+totalBlocks = chunkWidth**2*chunkColumns*chunkRows
+chunkSize = chunkWidth**2
+blocks = chunkColumns*chunkRows*chunkSize
+frames = totalBlocks*channelBlocks-1
+
+bpy.data.scenes[0].frame_end = frames
+
+for frame in range(frames):
+    clampedFrame = frame%blocks
+        
+    chunkIndex = clampedFrame%chunkSize
+    rowSize = chunkSize*chunkColumns
+
+    chunkBoundsPosition = [
+        (clampedFrame-chunkIndex)/chunkSize%chunkColumns*chunkWidth,
+        (clampedFrame-clampedFrame%rowSize)/rowSize*chunkWidth
+    ]
+
+    absolutePosition = [
+        chunkBoundsPosition[0] + chunkIndex%chunkWidth,
+        chunkBoundsPosition[1] + (chunkIndex-chunkIndex%chunkWidth)/chunkWidth
+    ]
+        
+    targetOffset = absolutePosition[1]+absolutePosition[0]*heightOld
+
+    mix_node_fac.default_value = (frame-frame%totalBlocks)/totalBlocks
+    image_node.frame_offset = targetOffset-frame-1
+
+    # drivers for the frame offset wouldn't update when rendering
+    mix_node_fac.keyframe_insert("default_value", frame=frame)
+    image_node.keyframe_insert("frame_offset", frame=frame)
