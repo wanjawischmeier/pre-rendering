f328fd83655fcfd26fe8360b367163b629cf0516
wanjawischmeier <69650445+wanjawischmeier@users.noreply.github.com>
2022-07-07 21:23:17 +0200

    basic rasterization and weighting working

--- src/unity/concept/Assets/Testing/BilinearInterpolation/TriangleRasterization.cs
+++ src/unity/concept/Assets/Testing/BilinearInterpolation/TriangleRasterization.cs
@@ -1,10 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TriangleRasterization : MonoBehaviour
 {
     public ComputeShader computeShader;
+    public Vector2Int resolution;
+    public bool wireframe;
     public float pointRadius = 0.05f;
 
     private Transform v0, v1, v2;
@@ -18,7 +18,7 @@ public class TriangleRasterization : MonoBehaviour
         v1 = GameObject.Find("v1").transform;
         v2 = GameObject.Find("v2").transform;
 
-        result = new RenderTexture(100, 100, 1);
+        result = new RenderTexture(resolution.x, resolution.y, 1);
         result.enableRandomWrite = true;
         result.filterMode = FilterMode.Point;
         GetComponent<MeshRenderer>().sharedMaterial.mainTexture = result;
@@ -26,6 +26,7 @@ public class TriangleRasterization : MonoBehaviour
         rasterizationKernel = computeShader.FindKernel("TriangleRasterization");
         computeShader.GetKernelThreadGroupSizes(rasterizationKernel, out threadGroupsX, out threadGroupsY, out _);
         computeShader.SetTexture(rasterizationKernel, "Result", result);
+        computeShader.SetVector("RESOLUTION", (Vector2)resolution);
     }
 
     private void OnDrawGizmos()
@@ -39,6 +40,9 @@ public class TriangleRasterization : MonoBehaviour
         computeShader.SetVector("V0", v0.position);
         computeShader.SetVector("V1", v1.position);
         computeShader.SetVector("V2", v2.position);
+
+        if (wireframe) computeShader.EnableKeyword("WIREFRAME");
+        else computeShader.DisableKeyword("WIREFRAME");
 
         computeShader.Dispatch(rasterizationKernel, (int)(result.width / threadGroupsX) + 1, (int)(result.height / threadGroupsY) + 1, 1);
     }
